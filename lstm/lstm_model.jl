2f3368ff7af82874c9c93912965f126e294c9bf3
OmerKirnap <okirnap@ku.edu.tr>
2016-02-24 21:08:31 +0200

    Create multilayer lstm

--- lstm/lstm_model.jl
+++ lstm/lstm_model.jl
@@ -5,7 +5,7 @@ data
 
 using Knet
 
-# Here I import my predefined train and test functions included in the repo 
+# Here I import my predefined train and test functions included in the repo
 include("/Users/omer/Documents/Developer/kdeep/rnn_native/trainer.jl")
 
 # Define the lstm model
@@ -17,4 +17,16 @@ include("/Users/omer/Documents/Developer/kdeep/rnn_native/trainer.jl")
   cell = input .* newmem + forget .* cell
   h = output .* tanh(cell)
   return h
+end
+
+
+@knet function lstmdrop(a; pdrop=0, hidden=0)
+  b = lstm(a; out=hidden)
+  return drop(b; pdrop=pdrop)
+end
+
+@knet function charlm(x; nlayer=0, embedding=0, hidden=0, pdrop=0, nchar=0)
+  a = wdot(x; out=embedding)
+  c = repeat(a; frepeat=:lstmdrop, nrepeat=nlayer, hidden=hidden, pdrop=pdrop)
+  return wbf(c; out=nchar, f:=soft)
 end
