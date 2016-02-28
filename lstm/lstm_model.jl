a99d838ab59492dd2910ce21272838ccc9c5ee9e
OmerKirnap <okirnap@ku.edu.tr>
2016-02-28 18:36:05 +0200

    use own lstm

--- lstm/lstm_model.jl
+++ lstm/lstm_model.jl
@@ -19,13 +19,14 @@ end
 
 
 @knet function lstmdrop(a; pdrop=0, hidden=0)
-  b = lstm(a; out=hidden)
+  b = clsclstm(a; out=hidden)
   return drop(b; pdrop=pdrop)
 end
 
 
 @knet function charlm(x; nlayers=0, embedding=0, hidden=0, pdrop=0, nchar=0)
     a = wdot(x; out=embedding)
-    c = repeat(a; frepeat=:lstmdrop, nrepeat=nlayers, hidden=hidden, pdrop=pdrop)
+
+    c = repeat(x; frepeat=:lstmdrop, nrepeat=nlayers, hidden=hidden, pdrop=pdrop)
     return wbf(c; out=nchar, f=:soft)
 end
