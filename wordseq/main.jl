e6ca8d3e273682ae3fa027da719db722afdb9ddf
OmerKirnap <okirnap@ku.edu.tr>
2016-03-10 21:52:56 +0200

    refactor validation

--- wordseq/main.jl
+++ wordseq/main.jl
@@ -17,13 +17,14 @@ function main()
 
   # create train and validation data
   trn = seqbatch(text, vocabulary, batchsize)
-  vld = seqbatch(val_text, val_vocab, batchsize)
+  vld = seqbatch(val_text, vocabulary, batchsize)
 
   # Create medium lstm
   info("Compiling the model...")
   mediumLSTM = compile(:genlstm; nlayer=2, embedding=512, hidden=650, pdrop=0.5, nchar=length(vocabulary))
   setp(mediumLSTM, lr=learning_rate)
 
+  prev_tst_err = 0
   info("Training starting...")
   for epoch=1:39
     train(mediumLSTM, trn, softloss;gclip=5)
